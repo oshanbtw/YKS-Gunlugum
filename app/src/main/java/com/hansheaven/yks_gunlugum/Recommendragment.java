ac9e4f82f36e21384309ac4d3062243b959f9894
oguzhan <oguzhanerkhrmn@gmail.com>
2022-01-25 04:52:21 +0300

    update - recommend page images

--- app/src/main/java/com/hansheaven/yks_gunlugum/Recommendragment.java
+++ app/src/main/java/com/hansheaven/yks_gunlugum/Recommendragment.java
@@ -21,24 +21,32 @@ public class Recommendragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_recommend, container, false);
 
         SliderView sliderView, sliderView2;
-        int[] images = {R.drawable.one,
-                R.drawable.two,
-                R.drawable.three,
-                R.drawable.four,
-                R.drawable.five,
-                R.drawable.six};
+        int[] images = {R.drawable.reklam,
+                R.drawable.reklam,
+                R.drawable.reklam,
+                R.drawable.reklam,
+                R.drawable.reklam,
+                R.drawable.reklam};
+
+        int[] images2 = {R.drawable.karekok,
+                R.drawable.limit,
+                R.drawable.bilgisarmal,
+                R.drawable.palme};
+
+
 
         sliderView = rootView.findViewById(R.id.image_slider);
         sliderView2 = rootView.findViewById(R.id.image_slider2);
 
         SliderAdapter sliderAdapter = new SliderAdapter(images);
+        SliderAdapter sliderAdapter2 = new SliderAdapter(images2);
 
         sliderView.setSliderAdapter(sliderAdapter);
         sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
         sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView.startAutoCycle();
 
-        sliderView2.setSliderAdapter(sliderAdapter);
+        sliderView2.setSliderAdapter(sliderAdapter2);
         sliderView2.setIndicatorAnimation(IndicatorAnimationType.WORM);
         sliderView2.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView2.startAutoCycle();
